491bb807868578e2816b95156ae54c4c30b4108f
maiquelmelo <maiquel.melo001@gmail.com>
2026-07-16 16:33:52 -0300

    V3.1: atualizar cache offline da produção inteligente

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE = "noeli-malta-v3-0-admin-finance-fix-3";
+const CACHE = "noeli-malta-v3-1-production-intelligent-1";
 const FILES = [
-  "./","./index.html","./manifest.json","./css/style.css","./css/dev-features.css","./css/v3-base.css","./css/v3-admin-finance.css","./js/app.js","./js/dev-features.js","./js/dev-ui.js","./js/dev-orders.js","./js/dev-production.js","./js/dev-notifications.js","./js/dev-workflow.js","./js/v3-base.js","./js/v3-delete-fix.js","./js/v3-admin-finance.js","./js/v3-admin-visible-fix.js","./assets/logo.png","./assets/favicon.png","./assets/splash.png"
+  "./","./index.html","./manifest.json","./css/style.css","./css/dev-features.css","./css/v3-base.css","./css/v3-admin-finance.css","./css/v3-1-production.css","./js/app.js","./js/dev-features.js","./js/dev-ui.js","./js/dev-orders.js","./js/dev-production.js","./js/dev-notifications.js","./js/dev-workflow.js","./js/v3-base.js","./js/v3-delete-fix.js","./js/v3-admin-finance.js","./js/v3-admin-visible-fix.js","./js/v3-1-production.js","./assets/logo.png","./assets/favicon.png","./assets/splash.png"
 ];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
