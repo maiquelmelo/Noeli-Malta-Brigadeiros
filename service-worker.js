6c641698336f0b32554fc3e5cc0aadaa4f5ecde6
maiquelmelo <maiquel.melo001@gmail.com>
2026-07-16 15:04:55 -0300

    chore(v3): atualizar cache da base profissional

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,7 @@
-const CACHE = "noeli-malta-v3-0-workflow-dev";
+const CACHE = "noeli-malta-v3-0-base-dev";
 const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./css/style.css",
-  "./css/dev-features.css",
-  "./js/app.js",
-  "./js/dev-features.js",
-  "./js/dev-ui.js",
-  "./js/dev-orders.js",
-  "./js/dev-production.js",
-  "./js/dev-notifications.js",
-  "./js/dev-workflow.js",
-  "./assets/logo.png",
-  "./assets/favicon.png",
-  "./assets/splash.png"
+  "./","./index.html","./manifest.json","./css/style.css","./css/dev-features.css","./css/v3-base.css","./js/app.js","./js/dev-features.js","./js/dev-ui.js","./js/dev-orders.js","./js/dev-production.js","./js/dev-notifications.js","./js/dev-workflow.js","./js/v3-base.js","./assets/logo.png","./assets/favicon.png","./assets/splash.png"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return response;
-    }).catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
-  );
-});
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim()});
+self.addEventListener("fetch",event=>{if(event.request.method!=="GET")return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("./index.html"))))});
